e3fbdd4bc0b1433fde2fd3a4f4263a70fa90070e
liukai <liukaiv5@gmail.com>
2022-04-06 20:39:40 +0800

    replace __div__ with __truediv__ in python

--- adolc/tangent.py
+++ adolc/tangent.py
@@ -31,101 +31,116 @@ class Tangent:
         AP.dependent(aJ)
         AP.trace_off()
     """
-    
+
     def __init__(self, x, xdot):
-        
+
         self.x = x
         self.xdot = xdot
-        
+
     def clone(self):
         if numpy.isscalar(self.x):
             x = self.x
-        
+
         else:
             x = self.x.clone()
-        
+
         if numpy.isscalar(self.xdot):
             xdot = self.xdot
-        
+
         else:
             xdot = self.xdot.clone()
-            
+
         return Tangent(x,xdot)
-        
+
     def __add__(self, rhs):
         retval = self.clone()
         retval += rhs
         return retval
-        
+
     def __sub__(self, rhs):
         retval = self.clone()
         retval -= rhs
-        return retval        
+        return retval
 
     def __mul__(self, rhs):
         retval = self.clone()
         retval *= rhs
         return retval
-        
+
     def __div__(self, rhs):
         retval = self.clone()
         retval /= rhs
         return retval
 
-    def __iadd__(self,rhs):
+    def __truediv__(self, rhs):
+        retval = self.clone()
+        retval /= rhs
+        return retval
+
+    def __iadd__(self, rhs):
         if isinstance(rhs, Tangent):
            self.x += rhs.x
            self.xdot += rhs.xdot
-        
+
         else:
             self.x += rhs
-            
+
         return self
-        
+
     def __isub__(self,rhs):
         if isinstance(rhs, Tangent):
            self.x -= rhs.x
            self.xdot -= rhs.xdot
-        
+
         else:
             self.x -= rhs
-            
-        return self        
-        
+
+        return self
+
     def __imul__(self,rhs):
         if isinstance(rhs, Tangent):
             self.xdot *= rhs.x
             self.xdot += self.x * rhs.xdot
             self.x *= rhs.x
-        
+
         else:
             self.xdot *= rhs
             self.x *= rhs
-            
+
         return self
-        
+
     def __idiv__(self,rhs):
         if isinstance(rhs, Tangent):
             self.x /= rhs.x
             self.xdot -= self.x * rhs.xdot
             self.xdot /= rhs.x
-        
+
         else:
             self.xdot /= rhs
             self.x /= rhs
-            
+
         return self
 
+    def __itruediv__(self, rhs):
+        if isinstance(rhs, Tangent):
+            self.x /= rhs.x
+            self.xdot -= self.x * rhs.xdot
+            self.xdot /= rhs.x
 
+        else:
+            self.xdot /= rhs
+            self.x /= rhs
+
+        return self
 
     def __radd__(self, lhs):
         return self + lhs
-        
+
     def __rmul__(self, lhs):
         return self * lhs
 
     def __str__(self):
         return "[%s,%s]"%(str(self.x),str(self.xdot))
-        
+
     def __repr__(self):
         return str(self)
